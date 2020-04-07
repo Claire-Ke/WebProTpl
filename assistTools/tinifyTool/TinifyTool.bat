2e7376845e431d20c212b7511f2e0a0093d49525
LMF <1227839175@qq.com>
2020-04-07 20:34:19 +0800

    This is a WEB project template.(2020)

--- assistTools/tinifyTool/TinifyTool.bat
+++ assistTools/tinifyTool/TinifyTool.bat
@@ -1,1 +1,1 @@
-node TinifyTool.js&pause
+node --experimental-import-meta-resolve --experimental-json-modules --experimental-repl-await --experimental-report --experimental-vm-modules --experimental-wasm-modules TinifyTool.js&pause
