ae11183d37c72c6957c7c65f2d394a033b0452eb
LMF <1227839175@qq.com>
2020-04-08 07:14:55 +0800

    This is a WEB project template.(2020)

--- configures/HTMLConfig.js
+++ configures/HTMLConfig.js
@@ -183,66 +183,70 @@ let htmlWebpackPluginA_arr = [
                     '/',
                 ],
                 icons: [
-                    {
-                        type: 'image/png',
-                        sizes: '48x48',
-                        href: '../static/ico/compressed/ico_48_48.png',
-                    },
-                    {
-                        type: 'image/png',
-                        sizes: '72x72',
-                        href: '../static/ico/compressed/ico_72_72.png',
-                    },
-                    {
-                        type: 'image/png',
-                        sizes: '96x96',
-                        href: '../static/ico/compressed/ico_96_96.png',
-                    },
-                    {
-                        type: 'image/png',
-                        sizes: '120x120',
-                        href: '../static/ico/compressed/ico_120_120.png',
-                    },
-                    {
-                        type: 'image/png',
-                        sizes: '128x128',
-                        href: '../static/ico/compressed/ico_128_128.png',
-                    },
-                    {
-                        type: 'image/png',
-                        sizes: '144x144',
-                        href: '../static/ico/compressed/ico_144_144.png',
-                    },
-                    {
-                        type: 'image/png',
-                        sizes: '150x150',
-                        href: '../static/ico/compressed/ico_150_150.png',
-                    },
-                    {
-                        type: 'image/png',
-                        sizes: '152x152',
-                        href: '../static/ico/compressed/ico_152_152.png',
-                    },
-                    {
-                        type: 'image/png',
-                        sizes: '167x167',
-                        href: '../static/ico/compressed/ico_167_167.png',
-                    },
-                    {
-                        type: 'image/png',
-                        sizes: '180x180',
-                        href: '../static/ico/compressed/ico_180_180.png',
-                    },
+                    /*
+                     {
+                     type: 'image/png',
+                     sizes: '48x48',
+                     href: '../static/ico/compressed/ico_48_48.png',
+                     },
+                     {
+                     type: 'image/png',
+                     sizes: '72x72',
+                     href: '../static/ico/compressed/ico_72_72.png',
+                     },
+                     {
+                     type: 'image/png',
+                     sizes: '96x96',
+                     href: '../static/ico/compressed/ico_96_96.png',
+                     },
+                     {
+                     type: 'image/png',
+                     sizes: '120x120',
+                     href: '../static/ico/compressed/ico_120_120.png',
+                     },
+                     {
+                     type: 'image/png',
+                     sizes: '128x128',
+                     href: '../static/ico/compressed/ico_128_128.png',
+                     },
+                     {
+                     type: 'image/png',
+                     sizes: '144x144',
+                     href: '../static/ico/compressed/ico_144_144.png',
+                     },
+                     {
+                     type: 'image/png',
+                     sizes: '150x150',
+                     href: '../static/ico/compressed/ico_150_150.png',
+                     },
+                     {
+                     type: 'image/png',
+                     sizes: '152x152',
+                     href: '../static/ico/compressed/ico_152_152.png',
+                     },
+                     {
+                     type: 'image/png',
+                     sizes: '167x167',
+                     href: '../static/ico/compressed/ico_167_167.png',
+                     },
+                     {
+                     type: 'image/png',
+                     sizes: '180x180',
+                     href: '../static/ico/compressed/ico_180_180.png',
+                     },
+                     */
                     {
                         type: 'image/png',
                         sizes: '192x192',
                         href: '../static/ico/compressed/ico_192_192.png',
                     },
-                    {
-                        type: 'image/png',
-                        sizes: '384x384',
-                        href: '../static/ico/compressed/ico_384_384.png',
-                    },
+                    /*
+                     {
+                     type: 'image/png',
+                     sizes: '384x384',
+                     href: '../static/ico/compressed/ico_384_384.png',
+                     },
+                     */
                     {
                         type: 'image/png',
                         sizes: '512x512',
@@ -412,66 +416,70 @@ let htmlWebpackPluginA_arr = [
                     '/',
                 ],
                 icons: [
-                    {
-                        type: 'image/png',
-                        sizes: '48x48',
-                        href: '../static/ico/compressed/ico_48_48.png',
-                    },
-                    {
-                        type: 'image/png',
-                        sizes: '72x72',
-                        href: '../static/ico/compressed/ico_72_72.png',
-                    },
-                    {
-                        type: 'image/png',
-                        sizes: '96x96',
-                        href: '../static/ico/compressed/ico_96_96.png',
-                    },
-                    {
-                        type: 'image/png',
-                        sizes: '120x120',
-                        href: '../static/ico/compressed/ico_120_120.png',
-                    },
-                    {
-                        type: 'image/png',
-                        sizes: '128x128',
-                        href: '../static/ico/compressed/ico_128_128.png',
-                    },
-                    {
-                        type: 'image/png',
-                        sizes: '144x144',
-                        href: '../static/ico/compressed/ico_144_144.png',
-                    },
-                    {
-                        type: 'image/png',
-                        sizes: '150x150',
-                        href: '../static/ico/compressed/ico_150_150.png',
-                    },
-                    {
-                        type: 'image/png',
-                        sizes: '152x152',
-                        href: '../static/ico/compressed/ico_152_152.png',
-                    },
-                    {
-                        type: 'image/png',
-                        sizes: '167x167',
-                        href: '../static/ico/compressed/ico_167_167.png',
-                    },
-                    {
-                        type: 'image/png',
-                        sizes: '180x180',
-                        href: '../static/ico/compressed/ico_180_180.png',
-                    },
+                    /*
+                     {
+                     type: 'image/png',
+                     sizes: '48x48',
+                     href: '../static/ico/compressed/ico_48_48.png',
+                     },
+                     {
+                     type: 'image/png',
+                     sizes: '72x72',
+                     href: '../static/ico/compressed/ico_72_72.png',
+                     },
+                     {
+                     type: 'image/png',
+                     sizes: '96x96',
+                     href: '../static/ico/compressed/ico_96_96.png',
+                     },
+                     {
+                     type: 'image/png',
+                     sizes: '120x120',
+                     href: '../static/ico/compressed/ico_120_120.png',
+                     },
+                     {
+                     type: 'image/png',
+                     sizes: '128x128',
+                     href: '../static/ico/compressed/ico_128_128.png',
+                     },
+                     {
+                     type: 'image/png',
+                     sizes: '144x144',
+                     href: '../static/ico/compressed/ico_144_144.png',
+                     },
+                     {
+                     type: 'image/png',
+                     sizes: '150x150',
+                     href: '../static/ico/compressed/ico_150_150.png',
+                     },
+                     {
+                     type: 'image/png',
+                     sizes: '152x152',
+                     href: '../static/ico/compressed/ico_152_152.png',
+                     },
+                     {
+                     type: 'image/png',
+                     sizes: '167x167',
+                     href: '../static/ico/compressed/ico_167_167.png',
+                     },
+                     {
+                     type: 'image/png',
+                     sizes: '180x180',
+                     href: '../static/ico/compressed/ico_180_180.png',
+                     },
+                     */
                     {
                         type: 'image/png',
                         sizes: '192x192',
                         href: '../static/ico/compressed/ico_192_192.png',
                     },
-                    {
-                        type: 'image/png',
-                        sizes: '384x384',
-                        href: '../static/ico/compressed/ico_384_384.png',
-                    },
+                    /*
+                     {
+                     type: 'image/png',
+                     sizes: '384x384',
+                     href: '../static/ico/compressed/ico_384_384.png',
+                     },
+                     */
                     {
                         type: 'image/png',
                         sizes: '512x512',
