4798100dd833a4653dd8c6dd4dcbd656d6cf7c91
LMF <1227839175@qq.com>
2020-04-11 12:43:12 +0800

    This is a WEB project template.(2020)

--- 更新项目模板的全部模块.bat
+++ 更新项目模板的全部模块.bat
@@ -1,1 +1,1 @@
-cd node_modules&npm update&cd..&node OutPackageJSON.js&ncu -u&npm install&pause
+cd node_modules&npm update&cd..&node OutPackageJSON.js&ncu -u&npm install&node OutPackageJSON.js&npm audit fix&node OutPackageJSON.js&pause
