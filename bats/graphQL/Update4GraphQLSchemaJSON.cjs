674158722023729def8d8cdccbf60b33c730a4f8
LMF <1227839175@qq.com>
2020-06-21 11:31:46 +0800

    This is a WEB project template.(2020) 介绍： 1、这是一个可以“即开即用”的WEB前端项目开发模板（脚手架）。 2、用于WEB前端项目的快速搭建、开发、编译、打包等WEB前端项目流程操作。 3、内置了大多数WEB前端项目都会有的各种配置。 4、内置支持Vue、TypeScript，也允许自定义扩展成自己想要的WEB项目开发环境（如扩展成：React、Angular等等）。 5、“src/js/tools”文件夹下有几个个人总结的JS工具类。

--- bats/graphQL/Update4GraphQLSchemaJSON.cjs
+++ bats/graphQL/Update4GraphQLSchemaJSON.cjs
@@ -1,8 +1,8 @@
 /**
- * Project: WebProTpl
+ * Project: sn-micro-front-web-project-template
  * Author：12278
- * Email：2726893248@qq.com
- * CreateDate：2020-01-01 00:00:00
+ * Email：1227839175@qq.com
+ * CreateDate：2020-06-01 00:00:00
  * IDE: WebStorm
  */
 
@@ -24,86 +24,216 @@ const fs = require( 'fs' ),
     http = require( 'http' );
 
 const jsonPath_strC = path.join( __dirname, '../../src/graphQL/GraphQL.Schema.json' ),
-    outContent_obj = JSON.parse( fs.readFileSync( jsonPath_strC ) );
+    outContent_obj = JSON.parse( fs.readFileSync( jsonPath_strC ) ),
+    mySchemaJSONGraphQL = JSON.stringify( {
+        query: fs.readFileSync( path.join( __dirname, '../../src/graphQL/api/SchemaJSON.graphql' ), {
+            encoding: 'utf8',
+        } ),
+    } );
 
-const option4Dev_objC = {
-        host: '192.168.1.100',
-        port: '8081',
-        path: '/graphql/schema.json',
-        method: 'GET',
-        headers: {
-            'User-Agent': 'My NodeJS for Update4GraphQLSchemaJSON',
-            'Accept-Encoding': 'gzip, deflate, br',
-            'Content-Type': 'application/json',
+// 获取"系统默认"的"GraphQL的Schema文档"
+if( true ){
+    const option4Dev_objC = {
+            host: '192.168.1.100',
+            port: '8081',
+            path: '/graphql/schema.json',
+            method: 'GET',
+            headers: {
+                'User-Agent': 'My NodeJS for Update4GraphQLSchemaJSON',
+                'Accept-Encoding': 'gzip, deflate, br',
+                'Content-Type': 'application/json',
+            },
         },
-    },
-    option4Dev2Natapp_objC = {
-        host: 'sn2020a.nat300.top',
-        port: '80',
-        path: '/graphql/schema.json',
-        method: 'GET',
-        headers: {
-            'User-Agent': 'My NodeJS for Update4GraphQLSchemaJSON',
-            'Accept-Encoding': 'gzip, deflate, br',
-            'Content-Type': 'application/json',
+        option4Dev2Natapp_objC = {
+            host: 'sn2020a.nat300.top',
+            port: '80',
+            path: '/graphql/schema.json',
+            method: 'GET',
+            headers: {
+                'User-Agent': 'My NodeJS for Update4GraphQLSchemaJSON',
+                'Accept-Encoding': 'gzip, deflate, br',
+                'Content-Type': 'application/json',
+            },
         },
-    },
-    option4Test_objC = {
-        host: '192.168.1.125',
-        port: '8080',
-        path: '/graphql/schema.json',
-        method: 'GET',
-        headers: {
-            'User-Agent': 'My NodeJS for Update4GraphQLSchemaJSON',
-            'Accept-Encoding': 'gzip, deflate, br',
-            'Content-Type': 'application/json',
+        option4Test_objC = {
+            host: '192.168.1.125',
+            port: '8080',
+            path: '/graphql/schema.json',
+            method: 'GET',
+            headers: {
+                'User-Agent': 'My NodeJS for Update4GraphQLSchemaJSON',
+                'Accept-Encoding': 'gzip, deflate, br',
+                'Content-Type': 'application/json',
+            },
         },
-    };
+        option4WWC_objC = {
+            host: '192.168.1.75',
+            port: '8081',
+            path: '/graphql/schema.json',
+            method: 'GET',
+            headers: {
+                'User-Agent': 'My NodeJS for Update4GraphQLSchemaJSON',
+                'Accept-Encoding': 'gzip, deflate, br',
+                'Content-Type': 'application/json',
+            },
+        };
 
-function Update4GraphQLSchemaJSON( opt = {} ){
-    return new Promise( ( resolve = () => {
-    }, reject = () => {
-    } ) => void ( http.get( opt, res => {
-                          const {
-                              statusCode,
-                          } = res;
-                          const contentType = res.headers[ 'content-type' ];
+    function Update4GraphQLSchemaJSON( opt = {} ){
+        return new Promise( ( resolve = () => {
+        }, reject = () => {
+        } ) => void ( http.get( opt, res => {
+                              const {
+                                  statusCode,
+                              } = res;
+                              const contentType = res.headers[ 'content-type' ];
 
-                          let error = null;
+                              let error = null;
 
-                          if( statusCode !== 200 ){
-                              error = new Error( 'Request Failed.\n' + `Status Code: ${ statusCode }` );
-                          }
-                          else if( !/^application\/json/.test( contentType ) ){
-                              error = new Error( 'Invalid content-type.\n' + `Expected application/json but received ${ contentType }` );
-                          }
-
-                          if( error ){
-                              // 使用响应数据释放内存
-                              res.resume();
-
-                              return error;
-                          }
-
-                          res.setEncoding( 'utf8' );
-
-                          let rawData = '';
-
-                          res.on( 'data', chunk => void ( rawData += chunk ) );
-
-                          res.on( 'end', () => {
-                              try{
-                                  resolve( JSON.parse( rawData ) );
+                              if( statusCode !== 200 ){
+                                  error = new Error( 'Request Failed.\n' + `Status Code: ${ statusCode }` );
                               }
-                              catch( e ){
-                                  reject( e );
+                              else if( !/^application\/json/.test( contentType ) ){
+                                  error = new Error( 'Invalid content-type.\n' + `Expected application/json but received ${ contentType }` );
                               }
-                          } );
-                      } )
-                      .on( 'error', e => void ( reject( e ) ) ) ) );
+
+                              if( error ){
+                                  // 使用响应数据释放内存
+                                  res.resume();
+
+                                  return error;
+                              }
+
+                              res.setEncoding( 'utf8' );
+
+                              let rawData = '';
+
+                              res.on( 'data', chunk => void ( rawData += chunk ) );
+
+                              res.on( 'end', () => {
+                                  try{
+                                      resolve( JSON.parse( rawData ) );
+                                  }
+                                  catch( e ){
+                                      reject( e );
+                                  }
+                              } );
+                          } )
+                          .on( 'error', e => void ( reject( e ) ) ) ) );
+    }
+
+    // 根据需要自己选取用哪个：
+    // option4Dev_objC
+    // option4Dev2Natapp_objC
+    // option4Test_objC
+    // option4WWC_objC
+    Update4GraphQLSchemaJSON( option4Dev2Natapp_objC )
+        .then( result => void ( fs.writeFileSync( jsonPath_strC, JSON.stringify( Object.assign( {}, outContent_obj, result ) ) ) ) )
+        .catch( e => void ( console.error( e ) ) );
 }
 
-// 根据需要自己选取用哪个：option4Dev_objC option4Dev2Natapp_objC option4Test_objC
-Update4GraphQLSchemaJSON( option4Dev2Natapp_objC )
-    .then( result => void ( fs.writeFileSync( jsonPath_strC, JSON.stringify( Object.assign( {}, outContent_obj, result ) ) ) ) )
-    .catch( e => void ( console.error( e ) ) );
+// 获取"自己定义"的更加全面详细的"GraphQL的Schema文档"
+if( false ){
+    const option4Dev_objC = {
+            host: '192.168.1.100',
+            port: '8081',
+            path: '/graphql/',
+            method: 'POST',
+            headers: {
+                'User-Agent': 'My NodeJS for Update4GraphQLSchemaJSON',
+                'Accept-Encoding': 'gzip, deflate, br',
+                'Content-Type': 'application/json',
+            },
+        },
+        option4Dev2Natapp_objC = {
+            host: 'sn2020a.nat300.top',
+            port: '80',
+            path: '/graphql/',
+            method: 'POST',
+            headers: {
+                'User-Agent': 'My NodeJS for Update4GraphQLSchemaJSON',
+                'Accept-Encoding': 'gzip, deflate, br',
+                'Content-Type': 'application/json',
+            },
+        },
+        option4Test_objC = {
+            host: '192.168.1.125',
+            port: '8080',
+            path: '/graphql/',
+            method: 'POST',
+            headers: {
+                'User-Agent': 'My NodeJS for Update4GraphQLSchemaJSON',
+                'Accept-Encoding': 'gzip, deflate, br',
+                'Content-Type': 'application/json',
+            },
+        },
+        option4WWC_objC = {
+            host: '192.168.1.75',
+            port: '8081',
+            path: '/graphql/',
+            method: 'POST',
+            headers: {
+                'User-Agent': 'My NodeJS for Update4GraphQLSchemaJSON',
+                'Accept-Encoding': 'gzip, deflate, br',
+                'Content-Type': 'application/json',
+            },
+        };
+
+    function Update4GraphQLSchemaJSON( opt = {} ){
+        return new Promise( ( resolve = () => {
+        }, reject = () => {
+        } ) => {
+            const req = http.request( opt, res => {
+                const {
+                    statusCode,
+                } = res;
+                const contentType = res.headers[ 'content-type' ];
+
+                let error = null;
+
+                if( statusCode !== 200 ){
+                    error = new Error( 'Request Failed.\n' + `Status Code: ${ statusCode }` );
+                }
+                else if( !/^application\/json/.test( contentType ) ){
+                    error = new Error( 'Invalid content-type.\n' + `Expected application/json but received ${ contentType }` );
+                }
+
+                if( error ){
+                    // 使用响应数据释放内存
+                    res.resume();
+
+                    return error;
+                }
+
+                res.setEncoding( 'utf8' );
+
+                let rawData = '';
+
+                res.on( 'data', chunk => void ( rawData += chunk ) );
+
+                res.on( 'end', () => {
+                    try{
+                        resolve( JSON.parse( rawData ) );
+                    }
+                    catch( e ){
+                        reject( e );
+                    }
+                } );
+            } );
+
+            req.on( 'error', e => void ( reject( e ) ) );
+
+            req.write( mySchemaJSONGraphQL );
+
+            req.end();
+        } );
+    }
+
+    // 根据需要自己选取用哪个：
+    // option4Dev_objC
+    // option4Dev2Natapp_objC
+    // option4Test_objC
+    // option4WWC_objC
+    Update4GraphQLSchemaJSON( option4Dev2Natapp_objC )
+        .then( result => void ( fs.writeFileSync( jsonPath_strC, JSON.stringify( Object.assign( {}, outContent_obj, result ) ) ) ) )
+        .catch( e => void ( console.error( e ) ) );
+}
